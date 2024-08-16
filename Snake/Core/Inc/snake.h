5ee4f69bf235463bf807cf155c26b74a2d056c9d
86thAuspiciousVerse <Unfinded@outlook.com>
2024-08-16 12:24:33 +0800

    世界上BUG最多的贪吃蛇游戏

---
+++ Snake/Core/Inc/snake.h
@@ -0,0 +1,40 @@
+#ifndef SNAKE_SNAKE_H
+#define SNAKE_SNAKE_H
+#include "main.h"
+#include "oled.h"
+#include <sys/types.h>
+//地图定义
+#define MAP_WIDTH 32
+#define MAP_HEIGHT 16
+//数据定义
+#define WALL 1
+#define SNAKE 2
+#define FOOD 3
+#define EMPTY 0
+//方向定义
+typedef enum {
+    UP = 100,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction;
+//点定义
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+}Point;
+//蛇定义
+typedef struct {
+    uint8_t length;
+    Direction direction;
+    Point body[MAP_WIDTH*MAP_HEIGHT];
+}Snake;
+
+void CreateMap();
+void PrintData();
+void GenerateFood();
+void InitSnake();
+void UpdateSnake();
+void ChangeDirection(Direction new_direction);
+#endif //SNAKE_SNAKE_H
+
---
+++ Snake/Core/Src/snake.c
@@ -0,0 +1,108 @@
+#include "snake.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+uint8_t map[MAP_WIDTH][MAP_HEIGHT];//地图数据，包含蛇和食物和墙
+Snake snake;//全局蛇变量
+bool directionChanged = false;
+//生成地图
+void CreateMap() {
+    OLED_Init();//初始化OLED
+    OLED_NewFrame();//新建帧
+    for (uint8_t i=0; i<MAP_WIDTH; i++) {
+        for (uint8_t j=0; j<MAP_HEIGHT; j++) {
+            if (i==0 || i== MAP_WIDTH-1 || j==0 || j== MAP_HEIGHT-1) {
+                map[i][j] = WALL;//墙
+            }else {
+                map[i][j] = EMPTY;//空白
+            }
+        }
+    }
+}
+//生成食物
+void GenerateFood() {
+    uint8_t x, y;
+    do {
+        x = rand() % (MAP_WIDTH - 2) + 1;
+        y = rand() % (MAP_HEIGHT - 2) + 1;
+    } while (map[x][y] != EMPTY);
+    map[x][y] = FOOD;
+}
+//初始化蛇
+void InitSnake(){
+    snake.length = 3;//蛇的长度
+    snake.direction = RIGHT;//蛇的方向
+    for (uint8_t i=0;i<snake.length;i++) {
+        snake.body[i].x = 3+i;
+        snake.body[i].y = 3;
+        map[snake.body[i].x][snake.body[i].y] = SNAKE;//设定为蛇
+    }//初始化蛇的位置,位置是固定的，在3,3到3,5
+    GenerateFood();//生成食物
+}
+
+void UpdateSnake() {
+    Point head = snake.body[0]; // 蛇头
+    switch (snake.direction) {
+        case UP:
+            head.y--;
+            break;
+        case DOWN:
+            head.y++;
+            break;
+        case LEFT:
+            head.x--;
+            break;
+        case RIGHT:
+            head.x++;
+            break;
+    } // 根据方向更新蛇头的位置
+
+    if (map[head.x][head.y] == WALL || map[head.x][head.y] == SNAKE) {
+        // 撞墙和撞自己
+        return;
+    }
+
+    if (map[head.x][head.y] == FOOD) {
+        // 吃到食物
+        snake.length++;
+        GenerateFood();
+    } else {
+        // 清除蛇尾
+        Point tail = snake.body[snake.length - 1];
+        map[tail.x][tail.y] = EMPTY;
+    }
+
+    // 移动蛇
+    for (uint8_t i = snake.length - 1; i > 0; i--) {
+        snake.body[i] = snake.body[i - 1];
+    }
+    snake.body[0] = head;
+    map[head.x][head.y] = SNAKE; // 更新蛇头的位置
+    directionChanged = false; // 重置标志位
+}
+
+void ChangeDirection(Direction new_direction) {
+        snake.direction = new_direction;
+        directionChanged = true; // 设置标志位
+}
+//打印数据
+void PrintData() {
+    for (uint8_t i=0; i<MAP_WIDTH; i++) {
+        for (uint8_t j=0; j<MAP_HEIGHT; j++) {
+            if (map[i][j]) {
+                OLED_DrawFilledRectangle(i*4,j*4,3,4,OLED_COLOR_NORMAL);
+            }
+            else {
+                OLED_DrawFilledRectangle(i*4,j*4,3,4,OLED_COLOR_REVERSED);
+            }
+        }
+    }
+    OLED_ShowFrame();
+}
+
+
+
+
